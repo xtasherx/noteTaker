41205d6bdf4a866dd65f74544eddbcfe1343b485
Tasha <xtasherx@gmail.com>
2020-10-06 22:11:59 -0500

    coded api routes page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 var PORT = process.env.PORT || 5000;
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app, fs);
+require("./routes/htmlRoutes")(app, fs);
 
 app.listen(PORT, () => {
   console.log(`App is listening on localhost: ${PORT}`);
